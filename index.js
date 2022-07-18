f55f7fd808de037e6690df55e77b437927d18a0c
Piers Dashfield <piersdashfield@hotmail.co.nz>
2022-07-18 14:53:15 +1000

    upload JS assessment to github

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+function validFormFieldInput() {
+    let flights = document.querySelector('#flighttype');
+    let leavingAirport = document.querySelector('#leavingfrom');
+    let arrivalAirport = document.querySelector('#destination');
+    let leaving = document.querySelector('#leavedate');
+    let arriving = document.querySelector('#returning');
+    let firstPassenger = document.querySelector('#passone');
+    let secondPassenger = document.querySelector('#passtwo');
+    let mobile = document.querySelector('#phone');
+    let email = document.querySelector('#phone');
+    let mailingAddress = document.querySelector('#physicaladdress');
+    console.log(validFormFieldInput);
+}
+
+
